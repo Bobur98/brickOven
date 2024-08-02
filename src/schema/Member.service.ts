2cd67aab3f8d93965a29ff0724fe5540468b61f0
Bobur98 <bmuhibullaev@mail.ru>
2024-08-03 00:24:41 +0900

    fix: merge

---
+++ src/schema/Member.service.ts
@@ -0,0 +1,250 @@
+import {
+  LoginInput,
+  Member,
+  MemberInput,
+  MemberInquiry,
+  MemberUpdateInput,
+} from "../libs/types/member";
+import Errors, { Message } from "../libs/Errors";
+import { HttpCode } from "../libs/Errors";
+import { MemberStatus, MemberType } from "../libs/enums/member.enum";
+import * as bcrypt from "bcryptjs";
+import { shapeIntoMongooseObjectId } from "../libs/config";
+import MemberModel from "../schema/Member.model";
+
+class MemberService {
+  private readonly memberModel;
+  constructor() {
+    this.memberModel = MemberModel;
+  }
+
+  /** SPA **/
+  public async getRestaurant(): Promise<Member> {
+    const result = await this.memberModel
+      .findOne({ memberType: MemberType.RESTAURANT })
+      .lean()
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    return result;
+  }
+
+  public async signup(input: MemberInput): Promise<Member> {
+    const salt = await bcrypt.genSalt();
+    input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+
+    try {
+      const result = await this.memberModel.create(input);
+      result.memberPassword = "";
+
+      return result.toJSON();
+    } catch (err) {
+      throw new Errors(HttpCode.BAD_REQUEST, Message.USED_NICK_NAME);
+    }
+  }
+
+  public async login(input: LoginInput): Promise<Member> {
+    const member = await this.memberModel
+      .findOne(
+        {
+          memberNick: input.memberNick,
+          memberStatus: { $ne: MemberStatus.DELETE },
+        },
+        { memberNick: 1, memberPassword: 1, memberStatus: 1 }
+      )
+      .exec();
+
+    if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+    else if (member.memberStatus === MemberStatus.BLOCK) {
+      throw new Errors(HttpCode.FORBIDDEN, Message.BLOCKED_USER);
+    }
+
+    const isMatch = await bcrypt.compare(
+      input.memberPassword,
+      member.memberPassword
+    );
+
+    if (!isMatch) {
+      throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
+    }
+
+    return await this.memberModel.findById(member._id).lean().exec();
+  }
+
+  public async addUserPoint(member: Member, point: number): Promise<Member> {
+    const memberId = shapeIntoMongooseObjectId(member._id);
+
+    return await this.memberModel
+      .findOneAndUpdate(
+        {
+          _id: memberId,
+          memberType: MemberType.USER,
+          memberStatus: MemberStatus.ACTIVE,
+        },
+        { $inc: { memberPoints: point } },
+        { new: true }
+      )
+      .exec();
+  }
+
+  public async getMemberDetail(member: Member): Promise<Member> {
+    const memberId = shapeIntoMongooseObjectId(member._id);
+    const result = await this.memberModel
+      .findOne({ _id: memberId, memberStatus: MemberStatus.ACTIVE })
+      .exec();
+
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    return result;
+  }
+
+  public async updateMember(
+    member: Member,
+    input: MemberUpdateInput
+  ): Promise<Member> {
+    const memberId = shapeIntoMongooseObjectId(member._id);
+    const result = await this.memberModel
+      .findOneAndUpdate({ _id: memberId }, input, { new: true })
+      .exec();
+
+    if (!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
+
+    return result;
+  }
+
+  public async getTopUsers(): Promise<Member[]> {
+    const result = this.memberModel
+      .find({ memberStatus: MemberStatus.ACTIVE, memberPoints: { $gte: 1 } })
+      .sort({ memberPoints: -1 })
+      .limit(4)
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    return result;
+  }
+
+  /** SSR**/
+  public async processSignup(input: MemberInput): Promise<Member> {
+    const exist = await this.memberModel
+      .findOne({
+        memberTime: MemberType.RESTAURANT,
+      })
+      .exec();
+    if (exist)
+      throw new Errors(
+        HttpCode.BAD_REQUEST,
+        Message.ONLY_ONE_RESTAURENT_ALLOWED
+      );
+
+    const salt = await bcrypt.genSalt();
+    input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+    try {
+      const result = await this.memberModel.create(input);
+      result.memberPassword = "";
+
+      return result;
+    } catch (err) {
+      throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+    }
+  }
+
+  public async processLogin(input: LoginInput): Promise<Member> {
+    const member = await this.memberModel
+      .findOne(
+        {
+          memberNick: input.memberNick,
+        },
+        // agar bu argumentlarni pass qilmasak bizga memberni hama malumotini qaytaradi, bizga esa memberNick va MemberPassword kerag holos
+        {
+          memberNick: 1,
+          memberPassword: 1,
+        }
+      )
+      .exec();
+    if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+
+    const isMatch = await bcrypt.compare(
+      input.memberPassword,
+      member.memberPassword
+    );
+    //const isMatch = input.memberPassword === member.memberPassword;
+
+    if (!isMatch)
+      throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
+
+    return await this.memberModel.findById(member._id).exec();
+  }
+
+  public async getUsers(inquiry: MemberInquiry): Promise<any> {
+    const pipeline: any[] = [];
+
+    // Match stage to filter by productCollection if provided
+    if (inquiry.memberStatus) {
+      pipeline.push({
+        $match: { memberStatus: inquiry.memberStatus },
+      });
+    }
+
+    // Match stage to search by productName if search query provided
+    if (inquiry.search) {
+      pipeline.push({
+        $match: { memberNick: { $regex: new RegExp(inquiry.search, "i") } },
+      });
+    }
+
+    // Pagination stages
+    pipeline.push(
+      { $skip: (inquiry.page - 1) * inquiry.limit },
+      { $limit: inquiry.limit }
+    );
+
+    // Pipeline for counting total documents without pagination
+    const countPipeline: any[] = [];
+
+    if (inquiry.memberStatus) {
+      countPipeline.push({
+        $match: { memberStatus: inquiry.memberStatus },
+      });
+    }
+
+    if (inquiry.search) {
+      countPipeline.push({
+        $match: { productName: { $regex: new RegExp(inquiry.search, "i") } },
+      });
+    }
+
+    countPipeline.push({ $count: "totalMembers" });
+
+    const [result, countResult] = await Promise.all([
+      this.memberModel.aggregate(pipeline).exec(),
+      this.memberModel.aggregate(countPipeline).exec(),
+    ]);
+
+    const totalProducts =
+      countResult.length > 0 ? countResult[0].totalProducts : 0;
+    const totalPages = Math.ceil(totalProducts / inquiry.limit);
+
+    if (!result) {
+      throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    }
+
+    const data = {
+      result,
+      totalProducts,
+      totalPages,
+    };
+
+    return data;
+  }
+  public async updateChosenUser(input: MemberUpdateInput): Promise<Member> {
+    const memberId = shapeIntoMongooseObjectId(input._id);
+    const result = await this.memberModel
+      .findByIdAndUpdate({ _id: memberId }, input, { new: true })
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
+
+    return result;
+  }
+}
+
+export default MemberService;
